2a4898cfbf35787286f37c8b939327e356f46904
Stanislaw Pankevich <s.pankevich@gmail.com>
2017-03-05 22:22:36 +0100

    Remove original struct from rustc-demangle.

--- lib/RustSymbolDemangling.h
+++ lib/RustSymbolDemangling.h
@@ -2,13 +2,6 @@
 #include <string>
 #include <vector>
 
-typedef struct {
-  std::string original;
-  std::string inner;
-  bool valid;
-  std::vector<std::string> elements;
-} Demangle;
-
 std::string RustSymbolDemangle(std::string &s, bool skipHashes = false);
 
 // These are exposed for unit testing.
